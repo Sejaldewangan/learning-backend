6f3d6f0796879c56a5f4bb193b873aa73ca154be
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-08-15 18:50:47 +0530

    b1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,22 @@
 import mongoose from "mongoose";
 import User from '../models/userModel.js'
 export async function getAllUsers(req,res) {
-    const {userName, userEmail,userPassword  }=req.body
-
    try {
-     const alredyUserExsists =await User.find({userEmail})
-    if (alredyUserExsists) {
-        res.json("user already exisists")
-    }
-const allUsers
+     const users = User.find().select("-userPassword")
+    return ( res.json({
+        message:"user found succesfull",
+        data: users
+    }))
    } catch (error) {
-    res.json({error:error.message})
+      return error.message ({
+        message:"user did not found succesfull",
+        data: null
+    })
+     
    }
+
+
+   
 
 }
 
